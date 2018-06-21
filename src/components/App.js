9fe8077e4dbd208c5e1097fe86cd15427f29d83c
Cedric Kisema <kisema@gmail.com>
2018-06-21 12:08:21 +0100

    adds banner with logo

--- src/components/App.js
+++ src/components/App.js
@@ -8,42 +8,46 @@ import ListWidgetContainer from '../components/ListWidgetContainer';
 // Add in styles
 import '../styles/App.css';
 import WeatherWidgetContainer from './Weather/WeatherWidgetContainer';
+import Banner from './Banner/Banner';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Widget heading="Weather">
-                    <WeatherWidgetContainer
-                        href="http://localhost:3001/weather/current" />
-                </Widget>
-                <Widget heading="Team Social">
-                    <ListWidgetContainer
-                        href="http://localhost:3001/team-social/all"
-                        heading=""
-                        rowspan={3} />
-                </Widget>
-                <Widget heading="Team News">
-                    <ListWidgetContainer
-                        href="http://localhost:3001/team-news/all"
-                        heading=""
-                        rowspan={3} />
-                </Widget>
-                <BirthdayWidgetContainer
-                    href="http://localhost:3001/birthday/next"
-                    heading="Next Birthday" />
-                <Widget heading="Holidays">
-                    <ListWidgetContainer
-                        href="http://localhost:3001/holidays/all"
-                        heading=""
-                        rowspan={3} />
-                </Widget>
-                <Widget heading="Overheard at transform">
-                    <h1>Tara : "It's a snack. You know, like cake- but chicken."</h1>
-                </Widget>
-                <BirthdayWidgetContainer
-                    href="http://localhost:3001/winning-behaviour/now"
-                    heading="This Month's Winning Behaviour" />
+            <div>
+                <Banner />
+                <div className="App">
+                    <Widget heading="Weather">
+                        <WeatherWidgetContainer
+                            href="http://localhost:3001/weather/current" />
+                    </Widget>
+                    <Widget heading="Team Social">
+                        <ListWidgetContainer
+                            href="http://localhost:3001/team-social/all"
+                            heading=""
+                            rowspan={3} />
+                    </Widget>
+                    <Widget heading="Team News">
+                        <ListWidgetContainer
+                            href="http://localhost:3001/team-news/all"
+                            heading=""
+                            rowspan={3} />
+                    </Widget>
+                    <BirthdayWidgetContainer
+                        href="http://localhost:3001/birthday/next"
+                        heading="Today's Birthday" />
+                    <Widget heading="Holidays">
+                        <ListWidgetContainer
+                            href="http://localhost:3001/holidays/all"
+                            heading=""
+                            rowspan={3} />
+                    </Widget>
+                    <Widget heading="Overheard at transform">
+                        <h1>Tara : "It's a snack. You know, like cake- but chicken."</h1>
+                    </Widget>
+                    <BirthdayWidgetContainer
+                        href="http://localhost:3001/winning-behaviour/now"
+                        heading="This Month's Winning Behaviour" />
+                </div>
             </div>
         );
     }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div>
+    <div className="title-logo">
+      <img src="./images/transform-logo.png" alt="" />
+    </div>
+  </div>
+);
+
+Banner.propTypes = {
+
+};
+
+export default Banner;
