82bfc67a55d7a1dbdd5fdca5b1b172dbaf6adc1f
charlene bastians <charlenebastians@hotmail.com>
2018-09-13 16:02:18 +0100

    graph reducer

---
+++ src/reducers/graph-reducer.js
@@ -0,0 +1,29 @@
+import { IS_LOADING, ISNT_LOADING, ADD_DATA } from '../actions/graph-actions';
+
+export const initialGraphState = {
+    loading: false,
+    data: []
+}
+
+export const graphReducer = (state = initialGraphState, action) => {
+    switch (action.type) {
+        case IS_LOADING: {
+            return {
+                ...state,
+                loading: action.switch
+            };
+        }
+        case ISNT_LOADING: {
+            return {
+                ...state,
+                loading: action.switch
+            };
+        }
+        case ADD_DATA: {
+            return {
+                ...state, 
+                data: action.newData
+            }
+        }
+    }
+}
