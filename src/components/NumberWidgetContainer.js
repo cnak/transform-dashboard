43d9cddb0ff1817c59ff271d8a7a4a1f36c86dd6
charlene bastians <charlenebastians@hotmail.com>
2018-09-13 17:52:16 +0100

    added map state and dispatch to props on number widget

--- src/components/NumberWidgetContainer.js
+++ src/components/NumberWidgetContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 // Import request module
 import axios from 'axios';
 
 // Import components
 import NumberWidget from '../components/NumberWidget';
+import { bindActionCreators } from '../../../../Library/Caches/typescript/3.0/node_modules/redux';
+import { is_loading, isnt_loading } from '../actions/action-helper';
 
 class NumberWidgetContainer extends Component {
     constructor() {
@@ -64,4 +68,23 @@ NumberWidgetContainer.propTypes = {
     href: React.PropTypes.string.isRequired
 }
 
-export default NumberWidgetContainer;
+const mapStateToProps = (state) => {
+    return {
+        loading: state.numberReducer.loading,
+        min: state.numberReducer.min,
+        max: state.numberReducer.max,
+        value: state.numberReducer.value
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        onIsLoading: is_loading,
+        OnIsntLoading: isnt_loading,
+        onGetMin: get_min,
+        onGetMax: get_max,
+        onGetValue: get_value
+    })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NumberWidgetContainer);
