0b1f5d8755b43b157966b5840bb5dbdf3154731e
charlene bastians <charlenebastians@hotmail.com>
2018-09-13 16:11:18 +0100

    updated graph widget container to use props from redux

--- src/components/GraphWidgetContainer.js
+++ src/components/GraphWidgetContainer.js
@@ -17,10 +17,10 @@ class GraphWidgetContainer extends Component {
         super();
 
         // Set initial state
-        this.state = {
-            loading: false,
-            data: []
-        }
+        // this.state = {
+        //     loading: false,
+        //     data: []
+        // }
 
         // Bind function to refer to component
         this.getData = this.getData.bind(this);
@@ -37,25 +37,31 @@ class GraphWidgetContainer extends Component {
     // Fetch new data
     getData() {
         // Tell the Widget component we're currently loading
-        this.setState({ loading: true });
+        onIsLoading()
 
         // Fetch data
         return axios.get(this.props.href)
             .then(response => {
                 // Update state with data
-                this.setState({ loading: false, data: response.data });
+                onIsntLoading(),
+                onAddData(response.data)
             })
             .catch(error => {
                 // At least tell the Widget component we have stopped loading
                 console.log(error);
-                this.setState({ loading: false });
+                onIsntLoading()
             });
     }
 
     render() {
         return (
             // Render the graph widget
-            <GraphWidget heading={this.props.heading} colspan={this.props.colspan} rowspan={this.props.rowspan} data={this.state.data} loading={this.state.loading} />
+            <GraphWidget 
+            heading={this.props.heading} 
+            colspan={this.props.colspan} 
+            rowspan={this.props.rowspan} 
+            data={this.props.data} 
+            loading={this.props.loading} />
         );
     }
 }
