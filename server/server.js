2cd8791570f02493f73c6b74d4744c359a76d869
Cedric Kisema <kisema@gmail.com>
2019-02-27 10:52:08 +0000

    adds ebignore

--- server/server.js
+++ server/server.js
@@ -3,17 +3,14 @@ const cors = require('cors')
 const app = express();
 
 const PORT = '8081'
-const API_URL = `http://localhost:${PORT}`
+// const API_URL = process.env${PORT}`
 
 app.use(cors())
 app.use(require('./controller'))
 
 app.use(function (req, res, next) {
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.listen(PORT, function () {
-  console.log(`Data being served from ${API_URL}`);
-});
+app.listen(PORT)
